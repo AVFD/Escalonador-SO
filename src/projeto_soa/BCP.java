7b024c9288c623a034170a5d02632f84f26df6e6
guilhermeip <guilherme.ianhez2@gmail.com>
2018-11-02 18:32:25 -0300

    primeiro commit

---
+++ src/projeto_soa/BCP.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto_soa;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author guilherme
+ */
+public class BCP {
+    private int id;
+    private int prioridade;
+    private String estado;
+    private int tempo;
+    private LinkedList<Integer> listaIO;
+    goias baitola
+    
+}
