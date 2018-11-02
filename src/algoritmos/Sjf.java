1b0b12d9970cb48daa930d8e1fa0c4dc624f7c87
AVFD <augustovicentefernandes@gmail.com>
2018-11-02 20:23:48 -0300

    teste Srj

--- src/algoritmos/Sjf.java
+++ src/algoritmos/Sjf.java
@@ -5,6 +5,8 @@
  */
 package algoritmos;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.LinkedList;
 import processo.BCP;
 
@@ -15,16 +17,35 @@ import processo.BCP;
 public class Sjf implements escalonador.Escalonador{
     private BCP processo;
     private LinkedList<BCP> listaProcessos;
-    private LinkedList<Integer> listaOrdemProcessos;
+    private int[] listaOrdemProcessos;
     
     @Override
     public void escalonar(){
+        int tamanhoLista = listaProcessos.size();
         
+        for (int j = 0; j < tamanhoLista; j++){
+            for (int k = 0; k < tamanhoLista; k++){
+                if (this.listaOrdemProcessos[j] == this.listaProcessos.get(k).getTempoTotal()){
+                    System.out.println("-> " + listaProcessos.get(k).getId());
+                    listaProcessos.remove(k);                    
+                }
+            }
+        }
     }
     
     @Override
     public void ordenar(){
+        int[] baseParaPrdenacao;
         
+        baseParaPrdenacao = new int[this.listaProcessos.size()];
+        
+        for (int i = 0; i < this.listaProcessos.size(); i++){
+            baseParaPrdenacao[i] = this.listaProcessos.get(i).getTempoTotal();
+        }
+        
+        Arrays.sort(baseParaPrdenacao);
+        
+        this.listaOrdemProcessos = baseParaPrdenacao.clone();
     }
     
 }
