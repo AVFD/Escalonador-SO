851b24b4456d0cd1180e6672943345053176119e
AVFD <augustovicentefernandes@gmail.com>
2018-11-04 20:39:17 -0200

    adicionando algoritmo de Prioridade

---
+++ src/algoritmos/Prioridade.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoritmos;
+
+import cpu.Processador;
+import processo.BCP;
+
+/**
+ *
+ * @author augusto
+ */
+public class Prioridade extends cpu.Escalonador {
+
+    @Override
+    public BCP escalonar() {
+        BCP bcp = new BCP();
+        int menorTempo;
+
+        retornarProcessosAptos(Processador.ciclo);
+
+        if (this.getListaAptos().isEmpty()) {
+            return null;
+        }
+
+        menorTempo = this.getListaAptos().get(0).getPrioridade();
+        bcp.copiaProcesso(this.getListaAptos().get(0));
+        for (BCP p : this.getListaAptos()) {
+            for (BCP p1 : this.getListaAptos()) {
+
+                if (menorTempo > p1.getPrioridade()) {
+                    menorTempo = p1.getPrioridade();
+                    bcp.copiaProcesso(p1);
+
+                }
+            }
+        }
+        return bcp;
+    }
+
+    @Override
+    public void retornarProcessosAptos(int ciclo) {
+        for (BCP p : this.getListaProcessos()) {
+            if (p.getTempoChegada()<= ciclo && !(processoJaApto(p.getId()))) {
+                this.getListaAptos().add(p);
+            }
+        }
+    }
+
+    @Override
+    public int returnIndexProcessoNaLista(int id) {
+        for (BCP p : this.getListaProcessos()) {
+            if (p.getId() == id) {
+                return getListaProcessos().indexOf(p);
+            }
+        }
+        return -1;
+    }
+    
+    private boolean processoJaApto(int indiceProcesso) {
+        boolean tem = false;
+        for (BCP p : this.getListaAptos()) {
+            if (p.getId() == indiceProcesso) {
+                tem = true;
+            }
+        }
+        return tem;
+    }
+    
+}
